ee523916d9049d029ec57db3169751d4f21fc4c3
IanB-dotcom <ianakubates@gmail.com>
2026-04-14 02:55:32 +0000

    Finished HTML, added all inputs and stats, soildified the idea and began work on my script.js where I defined the variables and added an error messege if the user inputs something invalid.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// add javascript here
+//
+
+let portfolio = [];
+
+let ticker = document.getElementById("ticker").value.toUpperCase;
+let buyPrice = parseFloat.document.getElementById("buyPrice").value;
+let currentPrice = parseFloat.document.getElementById("currentPrice").value;
+let shares = parseFloat.document.getElementById("shares").value;
+
+if (ticker = " " || isNaN(buyPrice) || isNaN(currentPrice) || isNan(shares))
+    document.getElementsById("msg").textContent = "Please enter a vaild input to all fields"
+
+
+function getReturn() {
+
+}
