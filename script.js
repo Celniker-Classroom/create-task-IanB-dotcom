5656520249ac51b1351d4eef14e7d54c5f95f65a
IanB-dotcom <ianakubates@gmail.com>
2026-04-16 20:51:13 +0000

    (I was out yesterday for track which is why theres a push absence). Today I debugged and fixed minor issues. I fixed basic symbols such as using "=" rather than "===". Additionally, I switched the order of various operations such as including my variable definition after my event listener. Overall I made many minor tweaks like this today.

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 let portfolio = [];
-let ticker = document.getElementById("ticker").value.toUpperCase();
-let buyPrice = parseFloat.document.getElementById("buyPrice").value;
-let currentPrice = parseFloat.document.getElementById("currentPrice").value;
-let shares = parseFloat.document.getElementById("shares").value;
-if (ticker = " " || isNaN(buyPrice) || isNaN(currentPrice) || isNan(shares)) {
-document.getElementsById("msg").textContent = "Please correctly fill all fields."
-return;
+
+
+
+document.getElementById("addBtn").addEventListener ("click", function() {
+
+if (ticker === "" || isNaN(buyPrice) || isNaN(currentPrice) || isNaN(shares)) {
+    document.getElementById("msg").textContent = "Please correctly fill all fields."
+    return;
 }
-document.getElementById("addButton").addEventListener ("click", function() {
+
+let ticker = document.getElementById("ticker").value.toUpperCase();
+let buyPrice = parseFloat(document.getElementById("buyPrice").value);
+let currentPrice = parseFloat(document.getElementById("currentPrice").value); 
+let shares = parseFloat(document.getElementById("shares").value); 
+
 portfolio.push ({ticker: ticker, shares: shares, buyPrice: buyPrice, currentPrice: currentPrice});
-document.getElementsById("msg").textContent = ticker + " was added to your portfolio.";
+document.getElementById("msg").textContent = ticker + " was added to your portfolio.";
 clearInputs();
 displayPortfolio(portfolio);
 analyzePortfolio(portfolio);
+});
+
 function analyzePortfolio(portfolioList){
-let totalInvested = 0;
-let totalValue = 0;
-for (let i = 0; i < portfolioList.length; i++){
-totalInvested += portfolioList[i].shares * portfolioList[i].buyPrice;
-totalValue += portfolioList[i].shares * portfolioList[i].currentPrice;
+    let totalInvested = 0;
+    let totalValue = 0;
+    for (let i = 0; i < portfolioList.length; i++){
+    totalInvested += portfolioList[i].shares * portfolioList[i].buyPrice;
+    totalValue += portfolioList[i].shares * portfolioList[i].currentPrice;
 }
+
 let gainLoss = totalValue - totalInvested;
 let returnPct = ((gainLoss / totalInvested) * 100).toFixed(2);
+
 if (gainLoss > 0){
 document.getElementById("result").textContent = "Portfolio up $" + gainLoss.toFixed(2) + " (" + returnPct + "%)";
 document.getElementById("result").style.color = "green";
@@ -35,4 +45,3 @@ document.getElementById("result").style.color = "black";
 document.getElementById("totalInvested").textContent = "Total Invested: $" + totalInvested.toFixed(2);
 document.getElementById("totalValue").textContent = "Current Value: $" + totalValue.toFixed(2);
 }
-})
